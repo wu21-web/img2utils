645bd82a7f2cd0b97cb02f537864b9aa7f93fc6d
wu21-web <wu2196674@icloud.com>
2026-09-12 09:35:08 +0900

    Parse flags after input path

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -18,11 +18,11 @@ func Run(name string, target convert.Format, args []string, stderr io.Writer) in
 	flags.StringVar(&output, "o", "", "output file")
 	flags.StringVar(&output, "O", "", "output file")
 	flags.Usage = func() {
-		fmt.Fprintf(stderr, "usage: %s input.%s [-o output.%s]\n", name, sourceExt(target), targetExt(target))
+		fmt.Fprintf(stderr, "usage: %s input.%s [-o output%s]\n", name, sourceExt(target), targetExt(target))
 		flags.PrintDefaults()
 	}
 
-	if err := flags.Parse(args); err != nil {
+	if err := flags.Parse(normalizeArgs(args)); err != nil {
 		return 2
 	}
 	if flags.NArg() != 1 {
@@ -45,6 +45,34 @@ func Run(name string, target convert.Format, args []string, stderr io.Writer) in
 		return 1
 	}
 	return 0
+}
+
+func normalizeArgs(args []string) []string {
+	flags := make([]string, 0, len(args))
+	positional := make([]string, 0, len(args))
+	rest := false
+
+	for i := 0; i < len(args); i++ {
+		arg := args[i]
+		switch {
+		case rest:
+			positional = append(positional, arg)
+		case arg == "--":
+			rest = true
+		case arg == "-o" || arg == "-O":
+			flags = append(flags, arg)
+			if i+1 < len(args) {
+				i++
+				flags = append(flags, args[i])
+			}
+		case strings.HasPrefix(arg, "-o=") || strings.HasPrefix(arg, "-O="):
+			flags = append(flags, arg)
+		default:
+			positional = append(positional, arg)
+		}
+	}
+
+	return append(flags, positional...)
 }
 
 func defaultOutput(input string, target convert.Format) string {
